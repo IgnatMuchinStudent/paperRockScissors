dce7f3419163fcc3190d8cb3440f656e624e60b2
Ignat <decl011@tut.by>
2020-06-29 15:22:44 +0300

    Updates

---
+++ precache-manifest.3d17f36567c4a35566c1dc874c7b0e22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08108ce5a0190d7811f99f76bf71a1aa",
+    "url": "/paperRockScissors/index.html"
+  },
+  {
+    "revision": "e74e4fe882df836ff3bb",
+    "url": "/paperRockScissors/static/css/main.34bce201.chunk.css"
+  },
+  {
+    "revision": "50d023ecc68028290a77",
+    "url": "/paperRockScissors/static/js/2.99df1d55.chunk.js"
+  },
+  {
+    "revision": "1d549b11cb9a0d87f2345e055e3cada2",
+    "url": "/paperRockScissors/static/js/2.99df1d55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e74e4fe882df836ff3bb",
+    "url": "/paperRockScissors/static/js/main.3c8cc005.chunk.js"
+  },
+  {
+    "revision": "2a3eb787fdc11d9efa35",
+    "url": "/paperRockScissors/static/js/runtime-main.00713d8d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.19cc320d62a56a67f8842d9f579cbfcc.js"
+  "/paperRockScissors/precache-manifest.3d17f36567c4a35566c1dc874c7b0e22.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/paperRockScissors/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
