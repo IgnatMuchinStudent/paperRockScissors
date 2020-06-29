3b565526b96364c2974c6a54c6b39ad4ebbc8dfe
Ignat <decl011@tut.by>
2020-06-29 15:20:06 +0300

    Updates

---
+++ precache-manifest.19cc320d62a56a67f8842d9f579cbfcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01233cb3d6c2676593f834b7fe962133",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e74e4fe882df836ff3bb",
+    "url": "/static/css/main.34bce201.chunk.css"
+  },
+  {
+    "revision": "50d023ecc68028290a77",
+    "url": "/static/js/2.99df1d55.chunk.js"
+  },
+  {
+    "revision": "1d549b11cb9a0d87f2345e055e3cada2",
+    "url": "/static/js/2.99df1d55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e74e4fe882df836ff3bb",
+    "url": "/static/js/main.3c8cc005.chunk.js"
+  },
+  {
+    "revision": "94cb6204ef3de284630f",
+    "url": "/static/js/runtime-main.f5c2c499.js"
+  }
+]);
